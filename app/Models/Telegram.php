21b4f5dca3dca7935c460a9899b57b75760d91a5
Ella Alinda <admin@ilolicon.com>
2024-01-28 17:24:59 +0800

    Product: use new login page

--- app/Models/Telegram.php
+++ app/Models/Telegram.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Telegram extends Model
+class Telegram extends NexusModel
 {
     use HasFactory;
 }
